ae7a504838bba59ad39972265d078d15b2888ec0
OmerTavor <58879865+OmerTavor@users.noreply.github.com>
2023-02-06 18:27:07 +0200

    A working version for segmenList() that returns the segment list with a limit (data to big fot request time) with solution A (loop and queries). I'll try soluiton B later (1 complex query).

--- server/src/routes/segments/segment-route-handler.ts
+++ server/src/routes/segments/segment-route-handler.ts
@@ -1,40 +1,71 @@
-import { Request, Response } from "express";
-import { handleResponseError } from "../route-handlers/route-error-handler";
-import { Collection, ObjectId } from "mongodb";
+import { Request, Response } from 'express';
+import { handleResponseError } from '../route-handlers/route-error-handler';
+import { Collection, ObjectId } from 'mongodb';
 import {
   ISegment,
   ISegmentGenderData,
   ISegmentMetaData,
-} from "../../common/types/db-models/segment";
-import { getDbWrapper } from "../../common/db/mongo-wrapper";
+} from '../../common/types/db-models/segment';
+import { getDbWrapper } from '../../common/db/mongo-wrapper';
+
+const _getSingleSegmentMetadata = async (
+  segmentItem: ISegment,
+  userCollection: Collection
+): Promise<ISegmentMetaData> => {
+  return (
+    await userCollection
+      .aggregate([
+        {
+          $match: { segment_ids: segmentItem._id },
+        },
+        {
+          $group: {
+            _id: '$gender',
+            userCount: { $sum: 1 },
+            avgIncome: { $avg: '$income_level' },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            userCount: { $sum: '$userCount' },
+            avgIncome: { $avg: '$avgIncome' },
+            topGender: { $max: '$_id' },
+          },
+        },
+        {
+          $addFields: {
+            name: segmentItem.name,
+          },
+        },
+      ])
+      .toArray()
+  )[0];
+};
 
 export async function segmentList(req: Request, res: Response): Promise<void> {
   try {
-    const segmentCollection: Collection = await (
-      await getDbWrapper()
-    ).getCollection("segments");
+    const [segmentCollection, userCollection] = await Promise.all([
+      (await getDbWrapper()).getCollection('segments'),
+      (await getDbWrapper()).getCollection('users'),
+    ]);
 
-    const dummyDataForSegement = await segmentCollection.find().map((segmentDocument):ISegmentMetaData => {
-      return {
-        _id: segmentDocument.id,
-        name: segmentDocument.name,
-        userCount: 7,
-        avgIncome: 50,
-        topGender: 1,
+    // I purposely added a limit to the segmentsArray in order to see a result in a normal timeframe,
+    // otherwise the queries will take unreasonable amount of time.
+    const segmentsArray = await segmentCollection.find().limit(5).toArray();
 
-      }    
-    }).toArray();
+    const segmentsMetadataArray = await Promise.all(
+      segmentsArray.map(
+        async (segmentItem) =>
+          await _getSingleSegmentMetadata(segmentItem, userCollection)
+      )
+    );
 
-    // todo TASK 1
-    // write this function to return { data: ISegmentMetaData[]; totalCount: number };
-    // where data is an array of ISegmentMetaData, and totalCount is the # of total segments
-
-    // the "users" collection
-    // const userCollection: Collection = await (await getDbWrapper()).getCollection('users');
-    // has a "many to many" relationship to the segment collection, check IUser interface or query the raw data.
-    // res.json({ success: true, data: ISegmentMetaData[], totalCount });
-
-    res.json({ success: true, data: dummyDataForSegement, totalCount: dummyDataForSegement.length });
+    res.json({
+      success: true,
+      data: segmentsMetadataArray,
+      totalCount: segmentsMetadataArray.length,
+    });
   } catch (error) {
     handleResponseError(
       `Get Segment List Error: ${error.message}`,
@@ -51,7 +82,7 @@ export async function getSegmentById(
   try {
     const segmentCollection: Collection = await (
       await getDbWrapper()
-    ).getCollection("segments");
+    ).getCollection('segments');
     const segment: ISegment = await segmentCollection.findOne({
       _id: new ObjectId(req.params.id as string),
     });
@@ -94,7 +125,7 @@ export async function getSegmentGenderData(
   try {
     const segmentCollection: Collection = await (
       await getDbWrapper()
-    ).getCollection("segments");
+    ).getCollection('segments');
 
     // todo TASK 2
     // write this function to return
